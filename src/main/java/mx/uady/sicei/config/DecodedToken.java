b30cb158b21dccda17d8eebaccb092a999557ce4
JavQuijano <JavQuijano@users.noreply.github.com>
2020-12-09 20:27:07 -0600

    Update DecodedToken.java

--- src/main/java/mx/uady/sicei/config/DecodedToken.java
+++ src/main/java/mx/uady/sicei/config/DecodedToken.java
@@ -12,14 +12,12 @@ public class DecodedToken {
   public String exp;
   public String iat;
   public String ext;
-  public String secret;
 
 
   public static DecodedToken getDecoded(String encodedToken) throws UnsupportedEncodingException {
       String[] pieces = encodedToken.split("\\.");
       String b64payload = pieces[1];
       String jsonString = new String(Base64.decodeBase64(b64payload), "UTF-8");
-      String secret = new String(Base64.decodeBase64(pieces[2]), "UTF-8");
       return new Gson().fromJson(jsonString, DecodedToken.class);
   }
 
