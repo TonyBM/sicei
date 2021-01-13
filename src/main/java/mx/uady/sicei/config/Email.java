469688f375fdce08861d5020dc8806d58822c9fe
JavQuijano <JavQuijano@users.noreply.github.com>
2021-01-12 22:56:14 -0600

    mas

--- src/main/java/mx/uady/sicei/config/Email.java
+++ src/main/java/mx/uady/sicei/config/Email.java
@@ -7,47 +7,51 @@ import javax.mail.MessagingException;
 import javax.mail.Session;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
-import com.google.api.client.util.Base64;
-import com.google.api.services.gmail.Gmail;
-import com.google.api.services.gmail.model.Message;
+// import com.google.api.client.util.Base64;
+// import com.google.api.services.gmail.Gmail;
+// import com.google.api.services.gmail.model.Message;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class Email {
 
-    @Autowired
-    private Gmail gmailInstance;
+    // @Autowired
+    // private Gmail gmailInstance;
 
-    public static MimeMessage createEmail(String to, String from, String subject, String bodyText)
-            throws MessagingException {
-        Properties props = new Properties();
-        Session session = Session.getDefaultInstance(props, null);
-        MimeMessage email = new MimeMessage(session);
+    // public static MimeMessage createEmail(String to, String from, String subject,
+    // String bodyText)
+    // throws MessagingException {
+    // Properties props = new Properties();
+    // Session session = Session.getDefaultInstance(props, null);
+    // MimeMessage email = new MimeMessage(session);
 
-        email.setFrom(new InternetAddress(from));
-        email.addRecipient(javax.mail.Message.RecipientType.TO, new InternetAddress(to));
-        email.setSubject(subject);
-        email.setText(bodyText);
-        return email;
-    }
+    // email.setFrom(new InternetAddress(from));
+    // email.addRecipient(javax.mail.Message.RecipientType.TO, new
+    // InternetAddress(to));
+    // email.setSubject(subject);
+    // email.setText(bodyText);
+    // return email;
+    // }
 
-    public static Message createMessageWithEmail(MimeMessage emailContent) throws MessagingException, IOException {
-        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-        emailContent.writeTo(buffer);
-        byte[] bytes = buffer.toByteArray();
-        String encodedEmail = Base64.encodeBase64URLSafeString(bytes);
-        Message message = new Message();
-        message.setRaw(encodedEmail);
-        return message;
-    }
+    // public static Message createMessageWithEmail(MimeMessage emailContent) throws
+    // MessagingException, IOException {
+    // ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+    // emailContent.writeTo(buffer);
+    // byte[] bytes = buffer.toByteArray();
+    // String encodedEmail = Base64.encodeBase64URLSafeString(bytes);
+    // Message message = new Message();
+    // message.setRaw(encodedEmail);
+    // return message;
+    // }
 
-    public Message sendMessage(String userId, MimeMessage emailContent) throws MessagingException, IOException {
-        Message message = createMessageWithEmail(emailContent);
-        message = gmailInstance.users().messages().send(userId, message).execute();
+    // public Message sendMessage(String userId, MimeMessage emailContent) throws
+    // MessagingException, IOException {
+    // Message message = createMessageWithEmail(emailContent);
+    // message = gmailInstance.users().messages().send(userId, message).execute();
 
-        System.out.println("Message id: " + message.getId());
-        System.out.println(message.toPrettyString());
-        return message;
-    }
+    // System.out.println("Message id: " + message.getId());
+    // System.out.println(message.toPrettyString());
+    // return message;
+    // }
 
 }
