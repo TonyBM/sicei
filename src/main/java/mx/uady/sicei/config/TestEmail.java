8a31ade9c7aeb14aa98de6034ff4b815bfe69418
JavQuijano <JavQuijano@users.noreply.github.com>
2021-01-12 23:29:46 -0600

    mas

--- src/main/java/mx/uady/sicei/config/TestEmail.java
+++ src/main/java/mx/uady/sicei/config/TestEmail.java
@@ -10,18 +10,22 @@ import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
-public class TestEmail {
-    static Properties props = new Properties();
+import org.springframework.context.annotation.Bean;
 
-    public TestEmail() {
+public class TestEmail {
+    private Properties props = new Properties();
+
+    @Bean
+    public TestEmail CrearTestEmail() {
         props.put("mail.smtp.host", "smtp.gmail.com");
         props.put("mail.smtp.socketFactory.port", "465");
         props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.port", "465");
+        return this;
     }
 
-    public static void send(final String from, final String password, String to, String sub, String msg) {
+    public void send(final String from, final String password, String to, String sub, String msg) {
         // get Session
         Session session = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
             protected PasswordAuthentication getPasswordAuthentication() {
